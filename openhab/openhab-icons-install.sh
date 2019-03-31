605c5d1d30bb2fd91bfa6e173ae1a32c56411bd6
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2019-03-31 17:23:18 +0200

    Update openhab-icons-install.sh

--- openhab/openhab-icons-install.sh
+++ openhab/openhab-icons-install.sh
@@ -11,11 +11,14 @@ ROOT_ICON="/etc/openhab2/icons/classic"
 
 # list of icons
 ARR_ICON=( "fan" "fan-off" "fan-on" )
-ARR_ICON=( "${ARR_ICON[@]}" "hygro" "hygro-0" "hygro-20"  "hygro-40" "hygro-60" "hygro-80" "hygro-95" )
 ARR_ICON=( "${ARR_ICON[@]}" "smoke" "smoke-off" "smoke-on" )
-ARR_ICON=( "${ARR_ICON[@]}" "temperature" "temperature-0" "temperature-15"  "temperature-20" "temperature-25" "temperature-30" "temperature-35" )
 ARR_ICON=( "${ARR_ICON[@]}" "vmc" "vmc-off" "vmc-on" "vmc_auto_off" "vmc_auto_on" )
+ARR_ICON=( "${ARR_ICON[@]}" "hygro" "hygro-0" "hygro-20"  "hygro-40" "hygro-60" "hygro-80" "hygro-95" )
+ARR_ICON=( "${ARR_ICON[@]}" "temperature" "temperature-0" "temperature-15"  "temperature-20" "temperature-25" "temperature-30" "temperature-35" )
 ARR_ICON=( "${ARR_ICON[@]}" "wifi" "wifi-0" "wifi-25"  "wifi-50" "wifi-75" )
+ARR_ICON=( "${ARR_ICON[@]}" "volt" "volt-0" "volt-210"  "volt-240" )
+ARR_ICON=( "${ARR_ICON[@]}" "amp" "amp-0" "amp-25" )
+ARR_ICON=( "${ARR_ICON[@]}" "watt" "watt-0" "watt-5000" )
 
 # loop to install icons, masks and alpha masks
 for ICON in "${ARR_ICON[@]}"
